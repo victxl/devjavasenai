4a3006450030383c952121af3527cd232ab024e5
Victor Henrique <victxl@gmail.com>
2023-03-30 19:47:37 -0300

    Create Variaveis.java

---
+++ Variaveis/src/com/variaveis/Variaveis.java
@@ -0,0 +1,17 @@
+package com.variaveis;
+
+public class Variaveis {
+
+	public static void main(String[] args) {
+		//declaração de variáveis
+	String nome;
+	int idade;
+	
+	// atribui valores às variàveis
+	nome = "Victor Henrique";
+	idade= 35;
+	
+	System.out.printf("Bom noite %s, você tem somente %d",nome,idade);
+
+}
+}
