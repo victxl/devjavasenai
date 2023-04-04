449541994b6ecacc9c77e2368816aab4a56edb4e
Victor Henrique <victxl@gmail.com>
2023-04-03 21:42:32 -0300

    orientação objeto

--- Usuario/src/com/usuario/Pessoa.java
+++ Usuario/src/com/usuario/Pessoa.java
@@ -1,5 +1,21 @@
 package com.usuario;
 
 public class Pessoa {
+	
+	//atributos
+	public String nome;
+	public String cidade;
+	public String profissao;
+	public int idade;
+	// métodos
+	
+	public void apresentar(){
+		System.out.printf("Ola, meu nome é %s. Moro na cidade %s, \ntenho %d anos, e trabalho com %s.\n",this.nome,this.cidade,this.idade,this.profissao);
+	}
+	
+	public void despedir() {
+		System.out.println("Foi um praser te conhcer. Até breve!");
+	}
+	
 
 }
--- Usuario/src/com/usuario/Principal.java
+++ Usuario/src/com/usuario/Principal.java
@@ -3,7 +3,26 @@ package com.usuario;
 public class Principal {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		// instancia o objeto
+		Pessoa usuario = new Pessoa();
+		Pessoa funcionario = new Pessoa();
+
+		// defindo valores para os atributos
+		usuario.nome = "ViCTOR";
+		usuario.idade = 35;
+		usuario.cidade = "Recanto";
+		usuario.profissao = "Fotografo";
+
+		funcionario.nome = "Victor2";
+		funcionario.idade = 37;
+		funcionario.cidade = "Goiânia";
+		funcionario.profissao = "Costureiro";
+
+		usuario.apresentar();
+		funcionario.despedir();
+		funcionario.apresentar();
+		funcionario.despedir();
+		
 
 	}
 
