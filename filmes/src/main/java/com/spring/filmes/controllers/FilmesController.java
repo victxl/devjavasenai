061510ccd09de20aaae325ff65cafe359bfb107f
Victor Henrique <victxl@gmail.com>
2023-06-20 21:40:22 -0300

    leitura do banco de dados

--- filmes/src/main/java/com/spring/filmes/controllers/FilmesController.java
+++ filmes/src/main/java/com/spring/filmes/controllers/FilmesController.java
@@ -4,42 +4,46 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 
 import com.spring.filmes.models.Filme;
 import com.spring.filmes.repository.FilmesRepository;
 
-
 @Controller
-
-
 public class FilmesController {
+	
 	@Autowired
 	private FilmesRepository fr;
 
-
-    @RequestMapping("/")
-    public String index() {
-    	
-        return "index";
-    }
-
-    @RequestMapping(value ="/cadastrar-filme",method=RequestMethod.GET)
-    public String cadastrarFilme() {
-        return "cadastrar-filme";
-    }
-
-    @RequestMapping("/lista-filmes")
-    public String listaFilmes() {
-        return "lista-filmes";
-    }
-    
-    @RequestMapping(value="/cadastrar-filme",method=RequestMethod.POST)
-    public String cadastrarFilme(Filme filme) {
-    	fr.save(filme);
-    	return "redirect:/cadastrar-filme";
-    	
-    }
-    
+	@RequestMapping("/")
+	public String index() {
+		return "index";
+	}
+	
+	@RequestMapping(value="/cadastrar-filme", method=RequestMethod.GET)
+	public String cadastrarFilme() {
+		return "cadastrar-filme";
+	}
+	
+	@RequestMapping(value="/cadastrar-filme", method=RequestMethod.POST)
+	public String cadastrarFilme(Filme filme) {
+		
+		fr.save(filme);
+		
+		return "redirect:/";
+		
+	}
+	
+	@RequestMapping("/lista")
+	public ModelAndView lista() {
+		
+		ModelAndView mv = new ModelAndView("lista");
+		
+		Iterable<Filme> filmes = fr.findAll();
+		mv.addObject("filmes", filmes);
+		
+		return mv;
+		
+	}
+	
 }
-
-
