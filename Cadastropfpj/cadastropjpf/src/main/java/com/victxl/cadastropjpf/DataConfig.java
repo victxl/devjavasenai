0e87be15e0b2399115e10b2ec8999002489298f2
Victor Henrique <victxl@gmail.com>
2023-06-26 21:51:40 -0300

    erro

--- Cadastropfpj/cadastropjpf/src/main/java/com/victxl/cadastropjpf/DataConfig.java
+++ Cadastropfpj/cadastropjpf/src/main/java/com/victxl/cadastropjpf/DataConfig.java
@@ -6,6 +6,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.orm.jpa.JpaVendorAdapter;
+import org.springframework.orm.jpa.vendor.Database;
 import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
 
 @Configuration
@@ -22,12 +23,16 @@ public class DataConfig {
     }
 
     @Bean
-    public JpaVendorAdapter jpaVendorAdapter() {
-        HibernateJpaVendorAdapter adapter = new HibernateJpaVendorAdapter();
-        adapter.setDatabasePlatform("org.hibernate.dialect.MySQL8Dialect");
-        adapter.setShowSql(true);
-        adapter.setGenerateDdl(true);
-        adapter.setPrepareConnection(true);
-        return adapter;
-    }
+public JpaVendorAdapter jpaVendorAdapter() {
+		
+		HibernateJpaVendorAdapter adapter = new HibernateJpaVendorAdapter();
+		
+		adapter.setDatabase(Database.MYSQL);
+		adapter.setShowSql(true);
+		adapter.setGenerateDdl(true);
+		adapter.setDatabasePlatform("org.hibernate.dialect.MySQLDialect");
+		adapter.setPrepareConnection(true);
+		
+		return adapter;
+	}
 }
